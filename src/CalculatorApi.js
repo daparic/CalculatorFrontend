42d970e543c1313ddd9ada9af228763d186dd5dd
truthadjuster <truthadjuster@gmail.com>
2023-06-23 02:38:44 +0800

    Use query strings for operands

--- src/CalculatorApi.js
+++ src/CalculatorApi.js
@@ -5,7 +5,7 @@ class CalculatorApi {
     }
 
     makeURL(number1, number2, operation) {
-        const resource = operation + "/" + number1 + "/" + number2;
+        const resource = operation + "?param1=" + number1 + "&param2=" + number2;
         return new URL(resource, this.serviceAddress);
     }
 
